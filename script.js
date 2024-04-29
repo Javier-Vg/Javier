c86139b9ff8cbb55519d35a23fe47c30b4f69f33
Javier-Vg <jjaviervgg@gmail.com>
2024-04-29 15:45:39 -0600

    Avance#5

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ btn1.addEventListener("click", function(){
 
 })
 
+
+//Barra dspelegable
+const iconoMenu = document.querySelector("#icono-menu"),
+    menu = document.querySelector("#menu");
+
+iconoMenu.addEventListener("click", function(e){
+    //Alternamos estilos para el menu y body
+    menu.classList.toggle("active");
+    document.body.classList.toggle("opacity");
+
+    //Altenamos su aributo "src para el icono del menu"
+    const rutaActual = e.target.getAttribute("src");
+
+    if (rutaActual == "/Javier/img/editar.png") {
+        e.target.setAttribute("src","/Javier/img2/editar.png")
+    }else{
+        e.target.setAttribute("src","/Javier/img/editar.png")
+    }
+})
