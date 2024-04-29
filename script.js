10160f7fde0870dbef15a636d122a5dd1a2d5ac6
Javier Vargas Guzman <Javier-Vg>
2024-04-28 22:34:51 -0600

    Avance#3

---
+++ script.js
@@ -0,0 +1,11 @@
+let nombre = document.getElementById("input1");
+let cont_nom = document.getElementById("cont-nombre");
+let btn1 = document.getElementById("btn1");
+
+
+
+btn1.addEventListener("click", function(){
+    cont_nom.innerHTML = nombre.value;
+
+})
+
